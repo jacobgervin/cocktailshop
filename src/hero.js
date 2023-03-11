7963ba59e7e7001def39912099b765db4efe1fac
jacobgervin <jacobgervin@gmail.com>
2023-03-11 01:05:33 +0100

    Hero Section

---
+++ src/hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HeroSection.css'; // Import your CSS file for styling
+import heroimage from './heroimg.png';
+
+function HeroSection() {
+  return (
+    <div className="hero-section">
+        <img src={heroimage} alt="cocktails" className='hero-image' />
+      <h1 className="hero-text">A GOOD COCKTAIL <br></br> DEMANDS GREAT LIQUOR</h1>
+      <h3 className='here-subtitle'>We have everything you need, to make the best cocktail</h3>
+    </div>
+  );
+}
+
+export default HeroSection;
