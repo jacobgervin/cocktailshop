072e494da490f37896901a888523b401c2e35df6
jacobgervin <jacobgervin@gmail.com>
2023-03-12 23:11:51 +0100

    cocktailshop

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navigation from './nav';
 import HeroSection from './hero';
+import Products from './products';
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HeroSection />
+  <Navigation />
+  <HeroSection />
+  <Products />
   </React.StrictMode>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
