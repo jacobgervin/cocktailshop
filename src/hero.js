71e3c289660d5a626f4229fe078a18fcf59ba394
jacobgervin <jacobgervin@gmail.com>
2023-03-16 20:57:42 +0100

    uppercase hero

--- src/hero.js
+++ src/hero.js
@@ -7,7 +7,7 @@ function HeroSection() {
     <div className="hero-section">
         <img src={heroimage} alt="cocktails" className='hero-image' />
       <h1 className="hero-text text-3xl font-black">A GOOD COCKTAIL <br></br> DEMANDS GREAT LIQUOR</h1>
-      <h3 className='here-subtitle font-extralight'>We have everything you need, to make the best cocktail</h3>
+      <h3 className='here-subtitle font-light uppercase'>Our cocktails only use the finest ingredients available</h3>
     </div>
   );
 }
